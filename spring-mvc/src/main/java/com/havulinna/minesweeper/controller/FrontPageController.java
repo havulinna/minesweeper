49073b44609b671eb99ae195a176a12f28b6e214
Teemu Havulinna <teemu.havulinna@houston-inc.com>
2015-04-09 15:16:52 +0300

    Moved styles to an external stylesheet. Split templates into reusable fragments.

--- spring-mvc/src/main/java/com/havulinna/minesweeper/controller/FrontPageController.java
+++ spring-mvc/src/main/java/com/havulinna/minesweeper/controller/FrontPageController.java
@@ -4,16 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.havulinna.minesweeper.model.Difficulty;
+import com.havulinna.minesweeper.controller.response.GameModelAndView;
 
 @Controller
 public class FrontPageController {
 
     @RequestMapping("/")
     public ModelAndView renderFrontPage() {
-        ModelAndView response = new ModelAndView("index");
-        response.addObject("difficulties", Difficulty.values());
-        return response;
+        return new GameModelAndView("index");
     }
-
 }
