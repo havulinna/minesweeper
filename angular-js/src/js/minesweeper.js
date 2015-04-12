cdd7f81b6b556fe072159cc14ba4125439b23fa8
Teemu Havulinna <teemu.havulinna@houston-inc.com>
2015-04-12 16:45:45 +0300

    Commenting and cleaning up JavaScript game logic.

--- angular-js/src/js/minesweeper.js
+++ angular-js/src/js/minesweeper.js
@@ -32,8 +32,13 @@ var minesweeper = {
         this.toggleFlag = function() {
             this._isFlagged = !this._isFlagged;
         };
+
+        /* Returns the number of mines placed in the squares next to this one */
         this.getNearbyMines = function() {
             return this._nearbyMines;
+        };
+        this.incrementNearbyMines = function() {
+            this._nearbyMines++;
         };
         this.isNeighbor = function(other) {
             if (other === this) {
@@ -49,6 +54,8 @@ var minesweeper = {
         this.height = height;
 
         this.rows = [];
+
+        // Creates a two-dimensional array and fills it with new Squares
         for (var row = 0; row < height; row++) {
             this.rows[row] = [];
             for (var col = 0; col < width; col++) {
@@ -56,20 +63,9 @@ var minesweeper = {
             }
         }
 
+        // Returns a new array containing all squares on this minefield
         this.getSquares = function() {
             return _.flatten(this.rows);
-        };
-
-        this.setMines = function(squaresWithMines) {
-            _.each(squaresWithMines, function(currentSquare) {
-                // Set mine in current square
-                currentSquare.setMine();
-
-                // Increment the square counters on neighbors
-                _.each(this.getNeighbors(currentSquare), function(neighbor) {
-                    neighbor._nearbyMines++;
-                }, this);
-            }, this);
         };
 
         this.getNeighbors = function(square) {
@@ -78,8 +74,21 @@ var minesweeper = {
             });
         };
 
-        var mines = _.sample(this.getSquares(), mineCount);
-        this.setMines(mines);
+        /* Sets the given squares as 'mines' and updates the mine counters of their neighbors */
+        this._setMines = function(squaresWithMines) {
+            _.each(squaresWithMines, function(currentSquare) {
+                // Set mine in current square
+                currentSquare.setMine();
+
+                // Increment the square counters on neighbors
+                _.each(this.getNeighbors(currentSquare), function(neighbor) {
+                    neighbor.incrementNearbyMines();
+                }, this);
+            }, this);
+        };
+
+        // Take the needed number of random squares and set mines to them
+        this._setMines(_.sample(this.getSquares(), mineCount));
     },
 
     MinesweeperGame : function(width, height, mineCount) {
@@ -104,12 +113,29 @@ var minesweeper = {
             return this._gameLost;
         };
 
+        this.isCompleted = function() {
+            /* Find any square that should still be opened. If there are no
+             * more squares left to open, the game is completed. */
+            var closedNonMine = _.find(
+                this._minefield.getSquares(),
+                function(square) {
+                    return !square.isMine() && !square.isOpen();
+                }
+            );
+            return !closedNonMine;
+        };
+
         this.toggleFlag = function(square) {
             if (!this.isGameLost() && !this.isGameWon() && !square.isOpen()) {
                 square.toggleFlag();
             }
         };
 
+        /* Opens the given square if the game is on and the square is not open nor flagged.
+         * 
+         * If the square itself nor its neighbors contain any mines, also the neighbors are
+         * opened recursively.
+         */
         this.openSquare = function(square) {
             if (this.isGameWon() || this.isGameLost() || square.isOpen() || square.isFlagged()) {
                 return;
@@ -119,6 +145,7 @@ var minesweeper = {
             if (square.isMine()) {
                 this._gameLost = true;
             } else {
+                // If there are no mines nearby, also the neighbors are opened recurively
                 if (square.getNearbyMines() === 0) {
                     var neighbors = this._minefield.getNeighbors(square);
                     _.each(neighbors, function(currentNeighbor) {
@@ -128,14 +155,7 @@ var minesweeper = {
                     }, this);
                 }
 
-                var closedNonMine = _.find(
-                    this._minefield.getSquares(),
-                    function(square) {
-                        return !square.isMine() && !square.isOpen();
-                    }
-                );
-
-                if (!closedNonMine) {
+                if (this.isCompleted()) {
                     this._gameWon = true;
                 }
             }
